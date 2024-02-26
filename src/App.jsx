c893415ecaafd1b9a984ab17d92ffa84c46fd395
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-02-27 00:13:00 +0700

    add title

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container flex justify-center">
+        <h1 className="font-bold text-4xl">To Do List</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
