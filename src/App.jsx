f5791f0b81d67f6c6e3cb919fa20794cba9a0e34
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-02-27 00:29:34 +0700

    setup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
   return (
-    <>
-      <div className="container flex justify-center">
-        <h1 className="font-bold text-4xl">To Do List</h1>
-      </div>
-    </>
+    <div className=" bg-slate-600 text-white flex justify-center h-lvh pt-10">
+      <h1 className="font-bold text-4xl">To Do List</h1>
+      <div></div>
+    </div>
   );
 }
 
